8cbc0ef063392d9ac9ce9a1ff6b2ff8ca6c7f80d
830503 <lixiaoqi19830503@gmail.com>
2021-11-03 14:18:08 +0100

    added json file

--- PostLoader.php
+++ PostLoader.php
@@ -4,22 +4,26 @@ declare(strict_types=1);
 class PostLoader 
 {
     private $post;
-    private $message=[];
+    private $message = [];
 
     function __construct(){
 
     }
 
     function loadPost(){
+        $jsonData = file_get_contents("posts.json");
+        $this->message = unserialize(json_decode($jsonData));
 
     }
 
     function savePost(){
-
+        array_push($this->message, $post);
+        $jsonData = json_encode(serialize($this->message));
+        file_put_contents("posts.json", $jsonData);
     }
 
     function getMessage(){
-        
+
     }
 
 }
